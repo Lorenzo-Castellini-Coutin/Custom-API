0fe5861742a50e428c785238ec3e9f44c3fa1fc0
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-14 17:46:12 -0400

    Finished most of the ClassDAO and MessageDAO, and added Flask.

--- CustomAPI.py
+++ CustomAPI.py
@@ -1,13 +1,16 @@
-import flask
+from flask import Flask, jsonify, request
 import sqlite3
 
-class UserDAO:
-  def __init__(self):
-    self.conn = sqlite3.connect('usersdatabase.db')
-    self.cursor = self.conn.cursor()
+app = Flask(__name__)
 
-  def addNewUser():
-    pass
+class UserDAO:
+  def __init__(self):  #self explains the behaviour for the rest of the class
+    self.conn = sqlite3.connect('usersdatabase.db')
+    self.cursor = self.conn.cursor()    #Object associated with db that allows to execute SQL commands
+
+  def addNewUser(self, user_id, firstname, lastname, datebirth, gender, phonenum, email, passw, prem):
+    self.cursor.execute("INSERT INTO users (user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address, password, is_premium)", (user_id, firstname, lastname, datebirth, gender, phonenum, email, passw, prem,))
+
   def getUserByEmail(self, email):
     self.cursor.execute("SELECT * FROM users WHERE email_address=?", (email,))
     return self.cursor.fetchall()
@@ -21,13 +24,14 @@ class MessageDAO:
     self.conn = sqlite3.connect('usersdatabase.db')
     self.cursor = self.conn.cursor()
 
-  def sendNewMessage():
-    pass
+  def sendNewMessage(self, subject, body, date):
+    self.cursor.execute("INSERT INTO messages (subject, body, date) VALUES (?,?,?)", (subject, body, date,))
+    
   def deleteMessage():
     pass
+  
   def getMessageById(self, messid):
-    self.cursor.execute("SELECT * FROM users WHERE")
+    self.cursor.execute("SELECT * FROM users WHERE m_id=?", (messid,))
   
 class Users:
-  
-#class Messages:
+  pass
