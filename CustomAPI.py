67ad5e70c25be4522089cd475170fc5a6b47a6eb
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-14 15:20:05 -0400

    Add files via upload

--- CustomAPI.py
+++ CustomAPI.py
@@ -1,3 +1,28 @@
 import flask
 import sqlite3
 
+class UserDAO:
+  def __init__(self):
+    self.conn = sqlite3.connect('usersdatabase.db')
+    self.cursor = self.conn.cursor()
+
+  def addNewUser():
+    pass
+  def getUserByEmail():
+    pass
+  def getUserById():
+    pass
+
+class MessageDAO:
+  def __init__(self):
+    pass
+  def sendNewMessage():
+    pass
+  def deleteMessage():
+    pass
+  def getMessageById():
+    pass
+  
+class Users:
+  
+#class Messages:
