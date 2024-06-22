7679c816be0e82b1b861d4d75977fa989b2a3afd
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-21 23:07:59 -0400

    Working version.....

--- Functions.py
+++ Functions.py
@@ -1,7 +1,4 @@
 import sqlite3
-from Models import UserDAO
-from Models import MessageDAO
-
 def db_connect():  #Function that creates connection with db
     conn = sqlite3.connect('usersdatabase.db')
     cursor = conn.cursor()
--- Routes.py
+++ Routes.py
@@ -1,36 +1,44 @@
 from flask import Flask, jsonify, request
-from Controllers import Users, Messages
+from Controllers.Users import Users
+from Controllers.Messages import Messages
 
 app = Flask(__name__)
 
-@app.route("/app")
-def hello():
-    return "Welcome!"
+@app.route('/app')
 
-@app.route("/app/adduser", methods=['POST'])
-def new_user():
+@app.route('/app/adduser', methods=['POST'])
+def add_user_input():
     if request.method == 'POST':
-        new_user_data = request.get_json()
-        Users.addNewUser(new_user_data)
+        user_data = request.get_json()
+        Users().addNewUser1(user_data)
+        return jsonify({'Message' : 'Data received succesfully'}), 200
+    else:
+        return jsonify({'Message' : 'Data not received properly'}), 405
 
-@app.route("/app/getuserbyid", methods=['POST'])
+@app.route('/app/getuserbyemail', methods=['GET'])
+def get_user_by_email_input():
+    if request.method == 'GET':
+        user_email = request.get_json()
+        user_info = Users().getUserByEmail1(user_email)
+        return jsonify({user_info}), 200
+    else:
+        return jsonify({'Message': 'User not found...'}), 405
+
+@app.route('/app/getuserbyid', methods = ['GET'])
 def get_user_by_id():
-    if request.method == 'POST':
-        userid_data = request.get_json()
-        Users.getUsersById(userid_data)
-
-@app.route("/app/getuserbyemail", methods=['GET']) 
-def get_user_by_email():
     pass
 
-@app.route("/app/sendmessage", methods=['POST'])
-def send_message():
+@app.route('/app/sendnewmessage', methods = ['POST'])
+def send_new_message():
     if request.method == 'POST':
-        message = request.get_json()
-        Messages.sendNewMessage(message)
-
-
-@app.route("/app/deletemessage", methods=['GET'])
+        new_message = request.get_json()
+        Messages().sendNewMessage(new_message)
+        return jsonify({'Message' : 'Message sent successfully'}), 200
+    else:
+        return jsonify({'Message' : 'Message not sent properly'}), 405
+    
+@app.route('/app/deleteMessage', methods = ['GET'])
 def delete_message():
-    pass
-
+    if request.method == 'GET':
+        message = request.get_json()
+        
