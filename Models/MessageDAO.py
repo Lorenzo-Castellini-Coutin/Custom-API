67fe79db4c245dbe4443e9665902843475802daf
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-21 23:07:24 -0400

    Working Version.....

--- Models/MessageDAO.py
+++ Models/MessageDAO.py
@@ -1,13 +1,22 @@
 from Functions import db_connect
 
 class MessageDAO:
-  db_connect()
-
-  def sendNewMessage(self, subject, body, date):
-    self.cursor.execute("INSERT INTO messages (subject, body, date) VALUES (?,?,?)", (subject, body, date,))
+  def sendNewMessage(self, new_message):
+    conn, cursor = db_connect()
+    query1 = '''INSERT INTO Message (message_id, sender_user_id, recipient_id, reply_id, subject, body, date, is_deleted)
+                VALUES(?,?,?,?,?,?,?,?)'''
     
-  def deleteMessage(self, subject):  #Ask how will the message be found?
-    self.cursor.execute("")
-  
+    cursor.execute(query1, (new_message['message_id'], new_message['sender_user_id'], new_message['date']))
+    
+    query2 = '''INSERT INTO Recipient (message_id, sender_user_id) VALUES(?,?)'''
+    cursor.execute(query2, (new_message['message_id'], new_message['sender_user_id']))
+    
+    conn.commit()
+    conn.close()
+    
+  def deleteMessage(self, subject, date):  #Ask how will the message be found?
+    conn, cursor = db_connect()
+
   def getMessageById(self, messid):
+    conn, cursor = db_connect()
     self.cursor.execute("SELECT * FROM users WHERE m_id=?", (messid,))
--- Models/UserDAO.py
+++ Models/UserDAO.py
@@ -1,25 +1,23 @@
 from Functions import db_connect
 
 class UserDAO:
-  def __init__(self, user_id, first_name, last_name, datebirth, gender, phone, email, passw, prem):
-    self.userid = user_id
-    self.first = first_name
-    self.last = last_name
-    self.birth = datebirth
-    self.gender = gender
-    self.phonenum = phone
-    self.email = email
-    self.passw = passw
-    self.prem = prem
+  def addNewUser2(self, user_data):
+    conn, cursor = db_connect()
+    
+    query = '''INSERT INTO users (user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address, password, is_premium)
+                VALUES(?,?,?,?,?,?,?,?,?)'''
+    
+    cursor.execute(query, (user_data['user_id'], user_data['firstname'], user_data['lastname'], user_data['birthdate'], user_data['gender'], user_data['phone'], user_data['email'], user_data['password'], user_data['premium']))
+    conn.commit()
+    conn.close()
 
-  db_connect()
-
-  def addNewUser(cursor, self):
-    cursor.execute("INSERT INTO users (user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address, password, is_premium)", (self.userid, self.first, self.last, self.birth, self.gender, self.phonenum, self.email, self.passw, self.prem,))
-
-  def getUserByEmail(cursor, self):
-    cursor.execute("SELECT * FROM users WHERE email_address=?", (self.email,))
-    return cursor.fetchall()
+  def getUserByEmail2(self, user_email):
+    conn, cursor = db_connect()
+    query = '''SELECT * FROM users WHERE email_address=? and is_deleted=0''', (user_email)
+    user_email2 = cursor.fetchall(query, user_email)
+    conn.commit()
+    conn.close()
+    return user_email2
   
   def getUserById(cursor, self):
     cursor.execute("SELECT * FROM users WHERE user_id=?", (self.id,))
