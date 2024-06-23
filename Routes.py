3be55bc138dea6c9db5a7550555b75d3e0ca79ac
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-22 20:39:52 -0400

    Fully functional

--- Routes.py
+++ Routes.py
@@ -6,39 +6,72 @@ app = Flask(__name__)
 
 @app.route('/app')
 
-@app.route('/app/adduser', methods=['POST'])
-def add_user_input():
+@app.route('/app/adduser', methods = ['POST'])
+def add_user():
     if request.method == 'POST':
         user_data = request.get_json()
         Users().addNewUser1(user_data)
         return jsonify({'Message' : 'Data received succesfully'}), 200
     else:
-        return jsonify({'Message' : 'Data not received properly'}), 405
+        return jsonify({'Message' : 'Data not received properly...'}), 405
 
-@app.route('/app/getuserbyemail', methods=['GET'])
-def get_user_by_email_input():
+@app.route('/app/getuserbyemail', methods = ['GET'])
+def get_user_by_email():
     if request.method == 'GET':
-        user_email = request.get_json()
+        user_data = request.get_json()
+        user_email = user_data['email']
         user_info = Users().getUserByEmail1(user_email)
-        return jsonify({user_info}), 200
+        if user_info:
+            return jsonify(list(user_info)), 200
+        else:
+            return jsonify({'Message' : 'User not found....'}), 405
     else:
-        return jsonify({'Message': 'User not found...'}), 405
+        return jsonify({'Message': 'Incorrect method....'}), 405
 
 @app.route('/app/getuserbyid', methods = ['GET'])
 def get_user_by_id():
-    pass
+    if request.method == 'GET':
+        user_data = request.get_json()
+        user_id = user_data['user_id']
+        user_info = Users().getUsersById1(user_id)
+        if user_info:
+            return jsonify(list(user_info)), 200
+        else:
+            return jsonify({'Message' : 'User not found...'}), 405
+    else:
+        return jsonify({'Message' : 'Incorrect method....'}), 405
 
 @app.route('/app/sendnewmessage', methods = ['POST'])
 def send_new_message():
     if request.method == 'POST':
         new_message = request.get_json()
-        Messages().sendNewMessage(new_message)
+        Messages().sendNewMessage1(new_message)
         return jsonify({'Message' : 'Message sent successfully'}), 200
     else:
-        return jsonify({'Message' : 'Message not sent properly'}), 405
+        return jsonify({'Message' : 'Message not sent properly...'}), 405
     
-@app.route('/app/deleteMessage', methods = ['GET'])
+@app.route('/app/getmessagebyid', methods = ['GET'])
+def get_message_by_id():
+    if request.method == 'GET':
+        user_data = request.get_json()
+        message_id = user_data['message_id']
+        message_info = Messages().getMessageById1(message_id)
+        if message_info:
+            return jsonify(list(message_info)), 200
+        else:
+            return jsonify({'Message' : 'Message not found...'}), 405
+    else:
+        return jsonify({'Message': 'Incorrect method.....'}), 405
+
+@app.route('/app/deletemessage', methods = ['GET'])
 def delete_message():
     if request.method == 'GET':
-        message = request.get_json()
+        del_message = request.get_json()
+        Messages().deleteMessage1(del_message)
+        if del_message:
+            return jsonify({'Message': 'Message deleted successfully'}), 200
+        else:
+            return jsonify({'Message': 'Message not found....'}), 405
+    else:
+        return jsonify({'Message' : 'Incorrect method...'}), 405
         
