9b92917be48f3242632330a18ba429729f240c94
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-27 21:19:05 -0400

    Additional fixes

--- Controllers/Messages.py
+++ Controllers/Messages.py
@@ -1,12 +1,40 @@
 from Models.MessageDAO import MessageDAO
+from flask import Flask, jsonify, request
+import json
 
 class Messages:
-  def sendNewMessage1(self, new_message):
-    MessageDAO().sendNewMessage2(new_message)
+  def sendNewMessage1(self, message):
+    new_message = MessageDAO().sendNewMessage2(message)
+    if new_message:  
+      return jsonify('Message sent succesfully.'), 200
+    else:
+      return jsonify('Message could not be sent.'), 500
 
-  def deleteMessage1(self, del_message):
-    MessageDAO().deleteMessage2(del_message)
+  def updateMessage1(self, new_message):
+    update_message = MessageDAO().updateMessage2(new_message)
+    if update_message:
+      return jsonify('Message updated successfully.'), 200
+    else:
+      return jsonify('Message was either already deleted or could not be updated.'), 500
+  
+  def deleteMessage1(self, message_id):
+    delete_message = MessageDAO().deleteMessage2(message_id)
+    if delete_message:  
+      return jsonify('Message deleted successfully.'), 200
+    else:
+      return jsonify('Message was either already deleted or could not be deleted.'), 400
 
   def getMessageById1(self, message_id):
     message_id1 = MessageDAO().getMessageById2(message_id)
-    return message_id1
+    if message_id1 is None:
+      return jsonify('Message was either not found or already deleted.'), 400
+    else:
+      message_template = {
+        'Sender_User_ID' : message_id1[0],
+        'Recipient_User_ID' : message_id1[1],
+        'Subject' : message_id1[2],
+        'Body' : message_id1[3],
+        'Date' : message_id1[4]
+    }
+      full_message = json.dumps(message_template, separators=(',', ':'), indent = 4)
+      return full_message
--- Controllers/Users.py
+++ Controllers/Users.py
@@ -1,14 +1,46 @@
 from Models.UserDAO import UserDAO
 from flask import Flask, jsonify, request
+import json
 
 class Users:
    def addNewUser1(self, user_data):
-      UserDAO().addNewUser2(user_data)
-      
-   def getUserByEmail1(self, user_email):
-      user_email1 = UserDAO().getUserByEmail2(user_email)
-      return user_email1
+      new_user = UserDAO().addNewUser2(user_data)
+      if new_user:
+         return jsonify('User added succesfully.'), 200
+      else:
+         return jsonify('Something went wrong with the info provided.'), 500
+
+   def updateUsers1(self, user_data):
+      update_user = UserDAO().updateUsers2(user_data)
+      if update_user:
+         return jsonify('User info updated successfully.'), 200
+      else:
+         return jsonify('User was either already deleted or not updated successfully.'), 500
 
    def getUsersById1(self, user_id):
-      user_id1 = UserDAO().getUserById2(user_id)
-      return user_id1
+      user_info = UserDAO().getUserById2(user_id)
+      if user_info is None:
+         return jsonify('User not found or was already deleted.'), 400
+      else:
+         user_template = {
+            "User_ID" : user_info[0],
+            "First Name" : user_info[1],
+            "Last Name" : user_info[2],
+            "Birth Day" : user_info[3],
+            "Gender" : user_info[4],
+            "Phone Number" : user_info[5],
+            "Email Address" : user_info[6],
+            "Membership" : user_info[7]
+         }
+         full_user_info = json.dumps(user_template, separators=(',', ':'), indent = 4)
+         return full_user_info
+
+   def deleteUsers1(self, user_id):
+         delete_user = UserDAO().deleteUsers2(user_id)
+         if delete_user:
+            return jsonify('User deleted succesfully.'), 200
+         else:
+            return jsonify('User was either already deleted or not deleted successfully.')
+      
+
+   
