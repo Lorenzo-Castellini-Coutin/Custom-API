a5f88aad4dca23edb5784cbe60f13d90ba75c4c8
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-27 21:19:30 -0400

    Additional fixes.

--- Models/MessageDAO.py
+++ Models/MessageDAO.py
@@ -1,35 +1,61 @@
 from Functions import db_connect
 
 class MessageDAO:
-  def sendNewMessage2(self, new_message):
+  def sendNewMessage2(self, message):
     conn, cursor = db_connect()
-    query1 = '''INSERT INTO messages (message_id, sender_user_id, recipient_user_id, reply_id, subject, body, date)
-                VALUES(?,?,?,?,?,?,?)'''
-    
-    cursor.execute(query1, (new_message['message_id'], new_message['sender_user_id'], new_message['recipient_user_id'], new_message['reply_id'], new_message['subject'], new_message['body'], new_message['date']))
-    
-    query2 = '''INSERT INTO recipient (message_id, sender_user_id, recipient_user_id) VALUES(?,?,?)'''
-    cursor.execute(query2, (new_message['message_id'], new_message['sender_user_id'], new_message['recipient_user_id']))
+    query1 = '''INSERT INTO messages (sender_user_id, recipient_user_id, reply_id, subject, body, date) VALUES(?,?,?,?,?,?)'''
+    cursor.execute(query1, (message['sender_user_id'], message['recipient_user_id'], message['reply_id'], message['subject'], message['body'], message['date']))
+
+    query2 = '''INSERT INTO recipient (sender_user_id, recipient_user_id) VALUES(?,?)'''
+    cursor.execute(query2, (message['sender_user_id'], message['recipient_user_id']))
     conn.commit()
+    changes = cursor.rowcount
     conn.close()
+    if changes <= 0:
+      return False
+    else: 
+      return True
     
+  def updateMessage2(self, new_message):
+    conn, cursor = db_connect()
+    query1 = 'UPDATE messages SET recipient_user_id=?, subject=?, body=?, date=? WHERE is_deleted=0 and message_id=?'
+    cursor.execute(query1, (new_message['recipient_user_id'], new_message['subject'], new_message['body'], new_message['date'], new_message['message_id']))
+
+    query2 = '''UPDATE recipient SET recipient_user_id=? WHERE is_deleted=0 and message_id=?'''
+    cursor.execute(query2, (new_message['recipient_user_id'], new_message['message_id']))
+    conn.commit()
+    changes = cursor.rowcount
+    conn.close()
+    if changes <= 0:
+      return False
+    else:
+      return True  
+
   def deleteMessage2(self, del_message):  
     conn, cursor = db_connect()
-    query1 = 'UPDATE messages SET is_deleted=1 WHERE subject=? and date=?'
-    cursor.execute(query1, (del_message['subject'], del_message['date']))
+    query1 = 'UPDATE messages SET is_deleted=1 WHERE message_id=?'
+    cursor.execute(query1, (del_message,))
     
     query2 = 'UPDATE recipient SET is_deleted=1 WHERE message_id=?'
-    cursor.execute(query2, (del_message['message_id'],))
+    cursor.execute(query2, (del_message,))
     conn.commit()
+    changes = cursor.rowcount
     conn.close()
-
+    if changes <= 0:
+      return False
+    else:
+      return True
 
   def getMessageById2(self, message_id):
     conn, cursor = db_connect()
-    query = 'SELECT sender_user_id, recipient_user_id, reply_id, subject, body, date FROM messages WHERE is_deleted=0 and message_id=?'
-    cursor.execute(query, (message_id,))
+    query1 = 'SELECT sender_user_id, recipient_user_id, subject, body, date FROM messages WHERE is_deleted=0 and message_id=?'
+    cursor.execute(query1, (message_id,))
     message_id2 = cursor.fetchone()
+    
+    query2 = 'UPDATE recipient SET is_read=1 WHERE is_deleted=0 and message_id=?'
+    cursor.execute(query2, (message_id,))
     conn.commit()
     conn.close()
     return message_id2
+    
   
--- Models/UserDAO.py
+++ Models/UserDAO.py
@@ -2,31 +2,50 @@ from Functions import db_connect
 
 class UserDAO:
   def addNewUser2(self, user_data):
-    conn, cursor = db_connect()
-    
-    query = '''INSERT INTO users (user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address, password, is_premium)
-                VALUES(?,?,?,?,?,?,?,?,?)'''
-    
-    cursor.execute(query, (user_data['user_id'], user_data['firstname'], user_data['lastname'], user_data['birthdate'], user_data['gender'], user_data['phone'], user_data['email'], user_data['password'], user_data['premium']))
+    conn, cursor = db_connect() 
+    query = '''INSERT INTO users (first_name, last_name, date_of_birth, gender, phone_number, email_address, password, is_premium) VALUES(?,?,?,?,?,?,?,?)'''
+    cursor.execute(query, (user_data['firstname'], user_data['lastname'], user_data['birthdate'], user_data['gender'], user_data['phone'], user_data['email'], user_data['password'], user_data['premium']))
     conn.commit()
+    changes = cursor.rowcount
     conn.close()
-
-  def getUserByEmail2(self, user_email):
+    if changes <= 0:
+      return False
+    else:
+      return True
+    
+    
+  def updateUsers2(self, user_data):
     conn, cursor = db_connect()
-    query = 'SELECT user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address FROM users WHERE is_deleted=0 and email_address=?'
-    cursor.execute(query, (user_email,))
-    user_email2 = cursor.fetchone()
+    query = '''UPDATE users SET first_name=?, last_name=?, date_of_birth=?, gender=?, phone_number=?, email_address=?, password=?, is_premium=? WHERE is_deleted=0 and user_id=?'''
+    cursor.execute(query, (user_data['firstname'], user_data['lastname'], user_data['birthdate'], user_data['gender'], user_data['phone'], user_data['email'], user_data['password'], user_data['premium'], user_data['user_id']))
     conn.commit()
+    changes = cursor.rowcount
     conn.close()
-    return user_email2
+    if changes <= 0:
+      return False
+    else:
+      return True
   
   def getUserById2(self, user_id):
     conn, cursor = db_connect()
-    query = 'SELECT user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address FROM users WHERE is_deleted=0 and user_id=?'
-    cursor.execute(query, ((user_id,)))
-    user_id2 = cursor.fetchone()
-    conn.commit()
+    query = 'SELECT user_id, first_name, last_name, date_of_birth, gender, phone_number, email_address, is_premium FROM users WHERE is_deleted=0 and user_id=?'
+    cursor.execute(query, (user_id,))
+    user_info2 = cursor.fetchone()
     conn.close()
-    return user_id2
+    return user_info2
+  
+  def deleteUsers2(self, user_id):
+    conn, cursor = db_connect()
+    query = 'UPDATE users SET is_deleted=1 WHERE user_id=?'
+    cursor.execute(query, (user_id,))
+    conn.commit()
+    changes = cursor.rowcount
+    conn.close()
+    if changes <= 0:
+      return False
+    else:
+      return True
+  
+  
    
 
