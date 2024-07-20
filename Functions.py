159eb54ab60d8332f3e78d306023ed8eec54462a
Lorenzo Castellini Coutin <lorenzo.castellini28@gmail.com>
2024-07-20 13:49:40 -0400

    Changed the functions.py to fit MySQL

--- Functions.py
+++ Functions.py
@@ -4,13 +4,16 @@ import os
 
 load_dotenv()
 
-db_connect = mysql.connector.connect (
-    host = os.getenv('SERVER_HOST'),
-    port = os.getenv('SERVER_PORT'),
-    user = os.getenv('DB_user'),
-    password = os.getenv('DB_password'),
-    database = os.getenv('DB_name')
-)
+def db_connect():
+    connection = mysql.connector.connect (
+        host = os.getenv('SERVER_HOST'),
+        port = os.getenv('SERVER_PORT'),
+        user = os.getenv('DB_user'),
+        password = os.getenv('DB_password'),
+        database = os.getenv('DB_name')
+    )
+    cursor = connection.cursor()
+    return connection, cursor
 
 
 
