27b46cdde6ae38591a60672bff399150c1120d8c
Lorenzo Castellini Coutin <lorenzo.castellini28@gmail.com>
2024-07-25 12:48:40 -0400

    Renaming queries...

--- Models/MessageDAO.py
+++ Models/MessageDAO.py
@@ -3,61 +3,61 @@ from Functions import db_connect
 class MessageDAO:
   def sendNewMessage2(self, message):
     conn, cursor = db_connect()
-    query1 = '''INSERT INTO messages (sender_user_id, recipient_user_id, reply_id, subject, body) 
+    messages_query = '''INSERT INTO messages (sender_user_id, recipient_user_id, reply_id, subject, body) 
                 VALUES(%s, %s, %s, %s, %s)'''
     
-    cursor.execute(query1, (message['sender_user_id'], message['recipient_user_id'], message['reply_id'], message['subject'], message['body']))
+    cursor.execute(messages_query, (message['sender_user_id'], message['recipient_user_id'], message['reply_id'], message['subject'], message['body']))
 
-    query2 = '''INSERT INTO recipients (sender_user_id, recipient_user_id) 
+    recipients_query = '''INSERT INTO recipients (sender_user_id, recipient_user_id) 
                 VALUES(%s, %s)'''
     
-    cursor.execute(query2, (message['sender_user_id'], message['recipient_user_id']))
+    cursor.execute(recipients_query, (message['sender_user_id'], message['recipient_user_id']))
     conn.commit()
     conn.close()
 
     
   def updateMessage2(self, new_message):
     conn, cursor = db_connect()
-    query1 = '''UPDATE messages SET recipient_user_id=%s, subject=%s, body=%s 
+    messages_query = '''UPDATE messages SET recipient_user_id=%s, subject=%s, body=%s 
                 WHERE is_deleted=0 and message_id=%s'''
     
-    cursor.execute(query1, (new_message['recipient_user_id'], new_message['subject'], new_message['body'], new_message['message_id']))
+    cursor.execute(messages_query, (new_message['recipient_user_id'], new_message['subject'], new_message['body'], new_message['message_id']))
 
-    query2 = '''UPDATE recipients SET recipient_user_id=%s 
+    recipients_query = '''UPDATE recipients SET recipient_user_id=%s 
                 WHERE is_deleted=0 and message_id=%s'''
     
-    cursor.execute(query2, (new_message['recipient_user_id'], new_message['message_id']))
+    cursor.execute(recipients_query, (new_message['recipient_user_id'], new_message['message_id']))
     conn.commit()
     conn.close()
 
 
   def deleteMessage2(self, del_message):  
     conn, cursor = db_connect()
-    query1 = '''UPDATE messages SET is_deleted=1 
+    messages_query = '''UPDATE messages SET is_deleted=1 
                 WHERE message_id=%s'''
     
-    cursor.execute(query1, (del_message,))
+    cursor.execute(messages_query, (del_message,))
     
-    query2 = '''UPDATE recipient SET is_deleted=1 
+    recipients_query = '''UPDATE recipient SET is_deleted=1 
                 WHERE message_id=%s'''
     
-    cursor.execute(query2, (del_message,))
+    cursor.execute(recipients_query, (del_message,))
     conn.commit()
     conn.close()
 
 
   def getMessageById2(self, message_id):
     conn, cursor = db_connect()
-    query1 = '''SELECT sender_user_id, recipient_user_id, subject, body, date FROM messages 
+    messages_query = '''SELECT sender_user_id, recipient_user_id, subject, body, date FROM messages 
                 WHERE is_deleted=0 and message_id=%s'''
 
-    cursor.execute(query1, (message_id,))
+    cursor.execute(messages_query, (message_id,))
     message_id2 = cursor.fetchone()
     
-    query2 = '''UPDATE recipient SET is_read=1 
+    recipients_query = '''UPDATE recipient SET is_read=1 
                 WHERE is_deleted=0 and message_id=%s'''
 
-    cursor.execute(query2, (message_id,))
+    cursor.execute(recipients_query, (message_id,))
     conn.commit()
     conn.close()
     return message_id2
